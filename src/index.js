f02cc72346623a3eadde723e12848a6c7f48919e
Pedro Lauro <pedro.lauro@gmail.com>
2018-05-20 19:41:44 -0300

    Redux + react-router + redux-logger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import App from './components/App/App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
+import createHistory from 'history/createBrowserHistory'
+// import { push } from 'react-router-redux'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
 
-ReactDOM.render((<BrowserRouter><App /></BrowserRouter>), document.getElementById('root'));
+import App from './components/App/App'
+import reducers from './reducers'
+import './index.css'
+
+const history = createHistory()
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer,
+  }),
+  applyMiddleware(routerMiddleware(history), logger),
+)
+
+// Now you can dispatch navigation actions from anywhere!
+// store.dispatch(push('/foo'))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
+)
