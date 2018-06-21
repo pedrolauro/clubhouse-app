2b95d65b5ece0d19f27beaba41d9d4b6d66a111f
Pedro Lauro <pedro.lauro@gmail.com>
2018-06-21 00:40:23 -0300

    Some cleaning and removing redux-logger from prd environment

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,3 @@
-// TODO: use environment variables for production
 export default {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
--- src/index.js
+++ src/index.js
@@ -14,10 +14,6 @@ import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
 import jss from 'jss'
 import preset from 'jss-preset-default'
 
-// fontawesome
-// import fontawesome from '@fortawesome/fontawesome'
-// import { faShip, faUsers, faCalendar, faHome, faSort } from '@fortawesome/fontawesome-free-solid'
-
 // material ui
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { MuiThemeProvider } from '@material-ui/core/styles'
@@ -27,16 +23,16 @@ import routes from './routes'
 import reducers from './reducers'
 import { theme, globalStyles } from './styles'
 
-
 jss.setup(preset())
 jss.createStyleSheet(globalStyles).attach()
-// fontawesome.library.add(faShip, faUsers, faCalendar, faHome, faSort)
 
-// TODO: remove logger on production
-// if (process.env.NODE_ENV !== 'production') {
-// }
 const history = createHistory()
-const store = createStore(reducers, applyMiddleware(routerMiddleware(history), thunk, logger))
+const middleware = [routerMiddleware(history), thunk]
+if (process.env.NODE_ENV !== 'production') {
+  // remove logger on production
+  middleware.push(logger)
+}
+const store = createStore(reducers, applyMiddleware(...middleware))
 
 ReactDOM.render(
   <Provider store={store}>
