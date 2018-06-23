6e4069f5de709a4cfb529e2f37737d1c838265d5
Pedro Lauro <pedro.lauro@gmail.com>
2018-06-23 13:50:28 -0300

    Cleaning up

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
-export const barcoToString = barco => `${barco.tipo} ${barco.classePeso} ${barco.cor}`
+export const barcoToString = (barco = {}) => `${barco.tipo || ''} ${barco.classePeso || ''} ${barco.cor || ''}`
 
-export const barcoToStringa = barco => `${barco.tipo} ${barco.classePeso} ${barco.cor}`
+// TODO: outros helpers
+export const barcoToStringa = (barco = {}) => `${barco.tipo || ''} ${barco.classePeso || ''} ${barco.cor || ''}`
