689cb6ce0a90e9fef7398e9c0b2f3b2ab90f4562
Pedro Lauro <pedro.lauro@gmail.com>
2018-06-24 03:27:33 -0300

    Immutable data

---
+++ src/reducers/data/immutable.js
@@ -0,0 +1,86 @@
+const tiposBarcos = ['1x', '2x', '2-', '2+', '4x', '4-', '4+', '8+']
+
+const categorias = [
+  {
+    $id: 'JUNIOR',
+    label: 'Junior',
+    limitByAge: false,
+  },
+  {
+    $id: 'ASPIRANTE',
+    label: 'Aspirante',
+    limitByAge: false,
+  },
+  {
+    $id: 'SENIOR',
+    label: 'Sênior',
+    limitByAge: false,
+  },
+  {
+    $id: 'MASTER_A',
+    label: 'Master A',
+    limitByAge: true,
+    limits: [27, 35],
+  },
+  {
+    $id: 'MASTER_B',
+    label: 'Master B',
+    limitByAge: true,
+    limits: [36, 42],
+  },
+  {
+    $id: 'MASTER_C',
+    label: 'Master C',
+    limitByAge: true,
+    limits: [43, 49],
+  },
+  {
+    $id: 'MASTER_D',
+    label: 'Master D',
+    limitByAge: true,
+    limits: [50, 54],
+  },
+  {
+    $id: 'MASTER_E',
+    label: 'Master E',
+    limitByAge: true,
+    limits: [55, 59],
+  },
+  {
+    $id: 'MASTER_F',
+    label: 'Master F',
+    limitByAge: true,
+    limits: [60, 64],
+  },
+  {
+    $id: 'MASTER_G',
+    label: 'Master G',
+    limitByAge: true,
+    limits: [65, 69],
+  },
+  {
+    $id: 'MASTER_H',
+    label: 'Master H',
+    limitByAge: true,
+    limits: [70, 74],
+  },
+  {
+    $id: 'MASTER_I',
+    label: 'Master I',
+    limitByAge: true,
+    limits: [75, 79],
+  },
+  {
+    $id: 'MASTER_J',
+    label: 'Master J',
+    limitByAge: true,
+    limits: [80, 200],
+  },
+]
+
+const initialData = {
+  tiposBarcos,
+  categorias,
+}
+
+export default (state = initialData) => state
--- src/reducers/data/index.js
+++ src/reducers/data/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import barcos from './barcos'
+import immutable from './immutable'
 
 export default combineReducers({
   barcos,
+  immutable,
 })
